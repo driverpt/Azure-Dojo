1430ce48eb71ca20c20a231a9a28b699c2b7a167
Luís Miguel Dias Duarte <lduarte@luisduarte.net>
2013-04-02 23:05:21 +0100

    Changed behaviour of the Worker Role, to pretend that its actually doing something

--- AzureCSharpSpike/WorkerRole1/WorkerRole.cs
+++ AzureCSharpSpike/WorkerRole1/WorkerRole.cs
@@ -1,4 +1,7 @@
+using System.Diagnostics;
+using System.Threading;
 using Microsoft.WindowsAzure.Storage.Queue;
+using WorkerRoleLib;
 
 namespace WorkerRole1
 {
@@ -6,8 +9,17 @@ namespace WorkerRole1
     {
         protected override void OnMessageReceived(CloudQueueMessage message)
         {
+            Trace.TraceInformation(@"Let's pretend we're working");
             string content = message.AsString;
             // TODO: Rest of the Implementation
+
+            Thread.Sleep(2000);
+        }
+
+        protected override void OnPostMessageReceived(CloudQueueMessage message)
+        {
+            base.OnPostMessageReceived(message);
+            Trace.TraceInformation(@"Message Processed");
         }
     }
 }
